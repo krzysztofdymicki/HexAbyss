2e028d11c08581c88858a4870e5b07fb3827b03a
unknown <krzysztof.dymicki.bp@gmail.com>
2021-06-05 19:54:01 +0200

    redux, redux-form, react-redux added

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
+const mainReducer = combineReducers({
+  disgForm: formReducer
+})
+
+const store = createStore(rootReducer)
+
+export default store
